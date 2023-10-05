fc394a18ee0db36345db2b1f9da40688490a6110
ni-jessica <jessica_ni@brown.edu>
2023-10-05 17:33:24 -0400

    feat:verify strong password

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, FormEvent, MouseEvent } from "react";
+import { useState, useEffect, FormEvent, MouseEvent } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -23,13 +23,26 @@ const theme = createTheme({
   },
 });
 
+const re = new RegExp(
+  "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$"
+);
+
 export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
+  const [isStrong, setIsStrong] = useState(false);
+  const [isValid, setIsValid] = useState(false);
+  const [first, setFirst] = useState<string>("");
+  const [last, setLast] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
+      firstName: data.get("firstName"),
+      lastName: data.get("lastName"),
       email: data.get("email"),
       password: data.get("password"),
     });
@@ -43,6 +56,18 @@ export default function SignUp() {
   ) => {
     event.preventDefault();
   };
+
+  useEffect(() => {
+    setIsStrong(re.test(password) && password.length > 9);
+
+    const valid =
+      first.length > 0 &&
+      last.length > 0 &&
+      email.length > 0 &&
+      password === confirmPassword;
+
+    setIsValid(valid);
+  }, [password, confirmPassword, isValid, isStrong]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -59,10 +84,23 @@ export default function SignUp() {
           <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
+          <Typography
+            aria-label="sign up header"
+            component="h1"
+            variant="h5"
+          >
             Sign up
           </Typography>
+          <Typography
+            aria-label="subheader"
+            component="p"
+            variant="subtitle1"
+            className="pt-2 text-gray-500 text-xs"
+          >
+            * Fields are required
+          </Typography>
           <Box
+            id="sign up form"
             component="form"
             noValidate
             onSubmit={handleSubmit}
@@ -77,6 +115,7 @@ export default function SignUp() {
                   fullWidth
                   id="firstName"
                   label="First Name"
+                  onChange={(event) => setFirst(event.target.value)}
                   autoFocus
                 />
               </Grid>
@@ -88,6 +127,7 @@ export default function SignUp() {
                   label="Last Name"
                   name="lastName"
                   autoComplete="family-name"
+                  onChange={(event) => setLast(event.target.value)}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -98,6 +138,7 @@ export default function SignUp() {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
+                  onChange={(event) => setEmail(event.target.value)}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -109,6 +150,9 @@ export default function SignUp() {
                   id="password"
                   autoComplete="new-password"
                   type={showPassword ? "text" : "password"}
+                  onChange={(event) =>
+                    setPassword(event.target.value)
+                  }
                   InputProps={{
                     endAdornment: (
                       <InputAdornment position="end">
@@ -136,6 +180,9 @@ export default function SignUp() {
                   label="Confirm your password"
                   id="confirm-password"
                   type={showPassword ? "text" : "password"}
+                  onChange={(event) =>
+                    setConfirmPassword(event.target.value)
+                  }
                   InputProps={{
                     endAdornment: (
                       <InputAdornment position="end">
@@ -155,12 +202,29 @@ export default function SignUp() {
                   }}
                 />
               </Grid>
+              <Typography
+                aria-label="password requirements"
+                component="div"
+                variant="inherit"
+                className="px-4"
+              >
+                <p className="-mb-2">Password must contain:</p>
+                <ul>
+                  <li>At least 10 characters</li>
+                  <li>At least 1 lowercase character</li>
+                  <li>At least 1 uppercase character</li>
+                  <li>At least 1 number</li>
+                  <li>At least 1 special character</li>
+                </ul>
+              </Typography>
             </Grid>
             <Button
+              aria-label="sign up button"
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              disabled={!isStrong || !isValid}
             >
               Sign Up
             </Button>
