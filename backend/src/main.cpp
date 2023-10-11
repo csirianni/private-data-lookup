80cd745f38e96ff53adb763072356ba5fa754035
Cedric Sirianni <cedric@sirianni.dev>
2023-10-11 17:54:59 -0400

    feat: create hello world application
    https://crowcpp.org/master/getting_started/your_first_application/

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -5,5 +5,14 @@
 #include "crow.h"
 
 int main(void) {
-    return EXIT_SUCCESS;
+    // delcare crow application
+    crow::SimpleApp app; 
+
+    // define endpoint at the root directory
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+
+    // set the port, set the app to run on multiple threads, and run the app
+    app.port(18080).multithreaded().run();
 }
