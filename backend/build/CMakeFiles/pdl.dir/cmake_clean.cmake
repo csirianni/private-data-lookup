6a039d6a9b83692436ed2c0325cf44f59be2f2b3
stellaljung <stella_ljung@brown.edu>
2023-10-16 14:04:27 -0400

    Initial start of database branch

---
+++ backend/build/CMakeFiles/pdl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pdl.dir/src/main.cpp.o"
+  "CMakeFiles/pdl.dir/src/main.cpp.o.d"
+  "pdl"
+  "pdl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pdl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
