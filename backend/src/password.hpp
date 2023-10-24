05bde14f4a9b40a60a22006e9f5833056fc4b8ad
Cedric Sirianni <cedric@sirianni.dev>
2023-10-24 13:08:25 -0400

    docs: change verb tense

--- backend/src/password.hpp
+++ backend/src/password.hpp
@@ -7,12 +7,12 @@
 namespace password
 {
     /**
-     * @brief Returns a set of passwords, where each password contains at least one lowercase 
+     * @brief Generate a set of passwords where each password contains at least one lowercase
      * letter, at least one uppercase letter, at least one digit, and at least special character.
-     * 
+     *
      * @param num_passwords The number of passwords to generate.
      * @param max_chars The maximum number of characters for a password.
-     * @return std::unordered_set<std::string> The passwords generated. 
+     * @return std::unordered_set<std::string> The passwords generated.
      */
     std::unordered_set<std::string> generatePasswords(int num_passwords, int max_chars);
 }
