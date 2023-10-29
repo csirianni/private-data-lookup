fca7a0a23c6b7d6370ff220c0454c0abd3479db5
stellaljung <stella_ljung@brown.edu>
2023-10-29 15:49:42 -0500

    feat: send get request

--- frontend/app/handlers.ts
+++ frontend/app/handlers.ts
@@ -1,21 +1,17 @@
 // Make API call to server to check if password was found in breached dataset
 export const checkSecurity = async () => {
-  const response = await fetch("http://localhost:18080", {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      "Access-Control-Allow-Headers": "*",
-    },
-  })
-    .then((res) => {
-      return res.json();
+  try {
+    const response = await fetch("http://localhost:18080", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Headers": "*",
+      },
     })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-      return {
-        status: "error",
-      };
-    });
-
-  return response;
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return { status: "error" };
+  }
 };
