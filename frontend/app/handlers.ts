e516e1c146656997ecc4e36aeed4f94f673e3eb0
Cedric Sirianni <cedric@sirianni.dev>
2023-10-29 17:13:04 -0400

    refactor: use 4-space tabs in frontend

--- frontend/app/handlers.ts
+++ frontend/app/handlers.ts
@@ -1,17 +1,17 @@
 // Make API call to server to check if password was found in breached dataset
 export const checkSecurity = async () => {
-  try {
-    const response = await fetch("http://localhost:18080", {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Access-Control-Allow-Headers": "*",
-      },
-    })
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return { status: "error" };
-  }
+    try {
+        const response = await fetch("http://localhost:18080", {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Access-Control-Allow-Headers": "*",
+            },
+        })
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return { status: "error" };
+    }
 };
