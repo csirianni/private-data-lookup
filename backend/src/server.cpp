b44c93f6688f20d23835706874b1ac192f54be01
stellaljung <stella_ljung@brown.edu>
2023-11-03 12:39:12 -0400

    feat: error check if request body is null

--- backend/src/server.cpp
+++ backend/src/server.cpp
@@ -34,6 +34,11 @@ namespace server
         // user's password passed in the post request
         std::string user_password = req.body;
 
+        if(user_password.empty()){ // no user password
+            response["status"] = "error";
+            return response;
+        }
+
         const bool in_breached_passwords = password_set.find(user_password) != password_set.end();
         if (in_breached_passwords)
         { // password is in the breached password set
