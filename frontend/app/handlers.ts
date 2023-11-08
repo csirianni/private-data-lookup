acbfa5f41d08bd523e4665bfbbba051f4f3f2c5a
ni-jessica <jessica_ni@brown.edu>
2023-11-07 19:35:56 -0500

    build: add libsodium

--- frontend/app/handlers.ts
+++ frontend/app/handlers.ts
@@ -1,15 +1,38 @@
+import {
+    crypto_pwhash_str,
+    crypto_pwhash_str_verify,
+    crypto_pwhash_OPSLIMIT_INTERACTIVE,
+    crypto_pwhash_MEMLIMIT_INTERACTIVE,
+} from "libsodium-wrappers-sumo";
+
 // Make API call to server to check if password was found in breached dataset
 export const checkSecurity = async (password: string) => {
     try {
-        const response = await fetch("http://localhost:18080/intersection", {
-            method: "POST",
-            mode: "cors",
-            headers: {
-                "Access-Control-Allow-Headers": "*", // cors setting
-                "Content-Type": "application/json"
-            },
-            body: password
-        })
+        // hash the password
+        const passwordHash = crypto_pwhash_str(
+            password,
+            crypto_pwhash_OPSLIMIT_INTERACTIVE,
+            crypto_pwhash_MEMLIMIT_INTERACTIVE
+        );
+        if (!crypto_pwhash_str_verify(passwordHash, password)) {
+            console.log("unable to hash password");
+            return { status: "error" };
+        }
+
+        // TODO: encrypt 
+
+        const response = await fetch(
+            "http://localhost:18080/intersection",
+            {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Access-Control-Allow-Headers": "*", // cors setting
+                    "Content-Type": "application/json",
+                },
+                body: password,
+            }
+        );
         const data = await response.json();
         return data;
     } catch (error) {
@@ -17,3 +40,8 @@ export const checkSecurity = async (password: string) => {
         return { status: "error" };
     }
 };
+
+// Encrypt password using libsodium's ristretto.
+function encrypt(password: string) {
+    return;
+}
