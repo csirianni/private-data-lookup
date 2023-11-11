564210857991c497af6577d9bd30f1f57c7f6a2f
Jessica Ni <98488267+ni-jessica@users.noreply.github.com>
2023-11-11 15:50:05 -0500

    build: add libsodium (#28)
    * build: add libsodium
    
    * refactor: remove hash for now

--- backend/conanfile.py
+++ backend/conanfile.py
@@ -9,6 +9,7 @@ class BackendRecipe(ConanFile):
         self.requires("crowcpp-crow/1.0+5")
         self.requires("sqlite3/3.42.0")
         self.requires("catch2/3.4.0")
+        self.requires("libsodium/1.0.18")
 
     def build_requirements(self):
         self.tool_requires("cmake/3.22.6")
