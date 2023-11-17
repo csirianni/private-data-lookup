ff51d1319e2a765eea68a3e6311711cb355aa78d
Cedric Sirianni <cedric@sirianni.dev>
2023-11-16 21:39:02 -0500

    refactor: reorder function definitions

--- backend/src/cryptography.cpp
+++ backend/src/cryptography.cpp
@@ -4,29 +4,6 @@
 namespace cryptography
 {
 
-    std::vector<std::string> encrypt(const std::unordered_set<std::string> &passwords, unsigned char *b)
-    {
-        std::vector<std::string> result;
-        result.reserve(passwords.size());
-
-        for (const auto &password : passwords)
-        {
-            // 1. hash password to point
-            unsigned char hash[crypto_generichash_BYTES];
-            // TODO: check if this is correct
-            crypto_generichash(hash, sizeof hash, (const unsigned char *)password.c_str(), password.length(), NULL, 0);
-
-            // 2. multiply by b
-            unsigned char encryptedPassword[crypto_core_ristretto255_BYTES];
-            crypto_scalarmult_ristretto255(encryptedPassword, b, hash);
-
-            std::string encryptedPasswordStr(reinterpret_cast<char *>(encryptedPassword));
-            result.push_back(encryptedPasswordStr);
-        }
-
-        return result;
-    }
-
     std::string encryptPassword(const std::string &password, unsigned char *b)
     {
         // hash password to point
@@ -34,9 +11,23 @@ namespace cryptography
         crypto_generichash(hash, sizeof hash, (const unsigned char *)password.data(), password.length(), NULL, 0);
         unsigned char point[crypto_core_ristretto255_BYTES];
         crypto_core_ristretto255_from_hash(point, hash);
-        // multiply by b
+
+        // encrypt password
         unsigned char encryptedPassword[crypto_core_ristretto255_BYTES];
         crypto_scalarmult_ristretto255(encryptedPassword, b, point);
         return std::string(encryptedPassword, encryptedPassword + crypto_core_ristretto255_BYTES);
+    }
+
+    std::vector<std::string> encrypt(const std::unordered_set<std::string> &passwords, unsigned char *b)
+    {
+        std::vector<std::string> result;
+        result.reserve(passwords.size());
+
+        for (const auto &password : passwords)
+        {
+            result.push_back(encryptPassword(password, b));
+        }
+
+        return result;
     }
 }
--- backend/src/cryptography.hpp
+++ backend/src/cryptography.hpp
@@ -4,6 +4,18 @@
 
 namespace cryptography
 {
+
+    /**
+     * @brief Encrypt the provided point using secret key b.
+     *
+     * This function converts the provided password to a point on an elliptic curve and then encrypts it using the provided secret key.
+     *
+     * @param password the password to encrypt
+     * @param b the secret key
+     * @return std::string the encrypted password
+     */
+
+    std::string encryptPassword(const std::string &password, unsigned char *b);
     /**
      * @brief Encrypt the provided set of passwords using secret key b.
      *
@@ -13,13 +25,4 @@ namespace cryptography
      */
     std::vector<std::string> encrypt(const std::unordered_set<std::string> &passwords,
                                      unsigned char *b);
-
-    /**
-     * @brief Encrypt the provided point using secret key b.
-     *
-     * @param password the point to encrypt
-     * @param b the secret key
-     * @return std::string the encrypted password
-     */
-    std::string encryptPassword(const std::string &password, unsigned char *b);
 }
