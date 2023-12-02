a7b097141e0f64dccb43a88b8a06de77a4666a8a
Cedric Sirianni <cedric@sirianni.dev>
2023-12-01 21:59:18 -0500

    build: include nlohmann json library

--- backend/conanfile.py
+++ backend/conanfile.py
@@ -10,6 +10,7 @@ class BackendRecipe(ConanFile):
         self.requires("sqlite3/3.42.0")
         self.requires("catch2/3.4.0")
         self.requires("libsodium/1.0.18")
+        self.requires("nlohmann_json/3.11.3")
 
     def build_requirements(self):
         self.tool_requires("cmake/3.22.6")
