32c655ee7459ec10b911ffc52868470a3a5a460b
Cedric Sirianni <cedric@sirianni.dev>
2023-12-01 22:07:33 -0500

    refactor: remove intermediary endpoints

--- backend/src/server.cpp
+++ backend/src/server.cpp
@@ -29,48 +29,6 @@ namespace server
         return response; });
     }
 
-    void passwords(crow::App<crow::CORSHandler> &app, const std::unordered_set<std::string> &passwords)
-    {
-        CROW_ROUTE(app, "/passwords")
-        ([passwords]()
-         {  crow::json::wvalue response;
-        std::vector<std::string> result;
-        result.reserve(passwords.size());
-        for (const auto &password : passwords) 
-        {
-            result.push_back(password);
-        }
-        response["passwords"] = result;
-        return response; });
-    }
-
-    void intersection(crow::App<crow::CORSHandler> &app, const std::unordered_set<std::string> &passwords)
-    {
-        CROW_ROUTE(app, "/intersection")
-            .methods("POST"_method)([passwords](const crow::request &req)
-                                    {
-        crow::json::wvalue response;
-
-        std::string user_password = req.body;
-        if (user_password.empty()) 
-        { 
-            response["status"] = "error";
-            return response;
-        }
-
-        const bool is_breached = passwords.find(user_password) != passwords.end();
-        if (is_breached)
-        { 
-            response["status"] = "fail";
-        }
-        else 
-        { 
-            response["status"] = "success";
-        }
-
-        return response; });
-    }
-
     void breachedPasswords(crow::App<crow::CORSHandler> &app, const std::vector<std::string> &passwords, unsigned char *b)
     {
         CROW_ROUTE(app, "/breachedPasswords")
@@ -92,5 +50,4 @@ namespace server
         
         return response; });
     }
-
 };
--- backend/src/server.hpp
+++ backend/src/server.hpp
@@ -17,22 +17,6 @@ namespace server
     void root(crow::App<crow::CORSHandler> &app);
 
     /**
-     * @brief Endpoint to show all breached passwords.
-     *
-     * @param app The crow server.
-     * @param passwords The set of all breached passwords.
-     */
-    void passwords(crow::App<crow::CORSHandler> &app, const std::unordered_set<std::string> &passwords);
-
-    /**
-     * @brief Endpoint to compute set intersection.
-     *
-     * @param app The crow server.
-     * @param passwords The set of all breached passwords.
-     */
-    void intersection(crow::App<crow::CORSHandler> &app, const std::unordered_set<std::string> &passwords);
-
-    /**
      * @brief Endpoint to serve the user password and breached passwords raised to the power of b.
      *
      * @param app The crow server.
