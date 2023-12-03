f6f325181114afc454bd0b9e980964406086caf7
Cedric Sirianni <cedric@sirianni.dev>
2023-12-02 22:49:50 -0500

    test: test Database class

---
+++ backend/test/database.cpp
@@ -0,0 +1,33 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_test_macros.hpp>
+#include <fstream>
+#include "database.hpp"
+
+TEST_CASE("Test Database class")
+{
+    std::ofstream file("test.db");
+    REQUIRE(file.is_open());
+
+    SECTION("Execute SELECT query")
+    {
+        database::Database db = database::Database("test.db");
+        REQUIRE_NOTHROW(db.execute("CREATE TABLE names (name TEXT);"));
+
+        std::vector<std::string> names = {"Cedric", "Stella", "Jessica"};
+        for (const auto &name : names)
+        {
+            REQUIRE_NOTHROW(db.execute("INSERT INTO names (name) VALUES ('" + name + "');"));
+        }
+
+        std::function<std::string(sqlite3_stmt *)> callback = [](sqlite3_stmt *stmt)
+        {
+            return std::string(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0)));
+        };
+        std::vector<std::string> result = db.execute("SELECT * FROM names;", callback);
+        CHECK(result == names);
+
+        db.close();
+    }
+
+    REQUIRE(std::remove("test.db") == 0);
+}
