eb3ceb2f9fb5fe1c23c79f0ffd2fee1bdc9fcaf9
stellaljung <stella_ljung@brown.edu>
2023-12-03 20:41:25 -0500

    test: test the contents of the names remain the same after reopening a database with the same file and build as default false
    BREAKING CHANGE: when the database file is initialized and reopened in the same test section then it passes, but not when the database file is reopened in a different section than initialization

--- backend/test/database.cpp
+++ backend/test/database.cpp
@@ -9,17 +9,20 @@ TEST_CASE("Test Database class")
     std::ofstream file(path);
     REQUIRE(file.is_open());
 
-    SECTION("Execute SELECT query")
+    SECTION("Execute SELECT query and then open a new database with the same file, database should be the same")
     {
+        // open a database
         database::Database db = database::Database(path);
         REQUIRE_NOTHROW(db.execute("CREATE TABLE names (name TEXT);"));
 
+        // insert names into the database
         std::vector<std::string> names = {"Cedric", "Stella", "Jessica"};
         for (const auto &name : names)
         {
             REQUIRE_NOTHROW(db.execute("INSERT INTO names (name) VALUES ('" + name + "');"));
         }
 
+        // check that the names are in the database
         std::function<std::string(sqlite3_stmt *)> callback = [](sqlite3_stmt *stmt)
         {
             return std::string(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0)));
@@ -27,18 +30,67 @@ TEST_CASE("Test Database class")
         std::vector<std::string> result = db.execute("SELECT * FROM names;", callback);
         CHECK(result == names);
 
+        // reopen the same database, build is false so the contents should be the same
+        database::Database reopen_db = database::Database(path);
+
+        // check if the names table is still there
+        std::function<bool(sqlite3_stmt *)> callback_names_table = [](sqlite3_stmt *stmt)
+        {
+            int count = atoi(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0)));
+            return count > 0;
+        };
+        std::vector<bool> result_names_table = reopen_db.execute("SELECT COUNT(*) FROM sqlite_schema WHERE name = 'names';", callback_names_table);
+        CHECK(result_names_table.front() == true);
+
+        // check if the specific names are still there
+        std::function<std::string(sqlite3_stmt *)> callback_each_name = [](sqlite3_stmt *stmt)
+        {
+            return std::string(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0)));
+        };
+        std::vector<std::string> result_each_name = reopen_db.execute("SELECT * FROM names;", callback_each_name);
+        CHECK(result_each_name == names);
+
         db.close();
+        reopen_db.close();
+    }
+
+    SECTION("Reopen the database"){
+        // reopen the same database, build is false so the contents should be the same
+        database::Database reopen_db = database::Database(path);
+
+        std::vector<std::string> names = {"Cedric", "Stella", "Jessica"};
+
+        // check if the names table is still there
+        std::function<bool(sqlite3_stmt *)> callback_names_table = [](sqlite3_stmt *stmt)
+        {
+            int count = atoi(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0)));
+            return count > 0;
+        };
+        std::vector<bool> result_names_table = reopen_db.execute("SELECT COUNT(*) FROM sqlite_schema WHERE name = 'names';", callback_names_table);
+        CHECK(result_names_table.front() == true);
+
+        // check if the specific names are still there
+        std::function<std::string(sqlite3_stmt *)> callback_each_name = [](sqlite3_stmt *stmt)
+        {
+            return std::string(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0)));
+        };
+        std::vector<std::string> result_each_name = reopen_db.execute("SELECT * FROM names;", callback_each_name);
+        CHECK(result_each_name == names);
+
+        reopen_db.close();
     }
 
     SECTION("Rebuild the database of names, database should be empty")
     {
         database::Database db = database::Database(path, true);
 
-        // callback function to check if the database is empty
+        // callback function to capture the counts of executing the query
         std::function<bool(sqlite3_stmt *)> callback = [](sqlite3_stmt *stmt)
         {
             int count = atoi(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0)));
-            return count == 0;
+
+            // count == 0 since there is nothing in the database
+            return (count == 0);
         };
 
         // query the sqlite_schema table to check if the database is empty
