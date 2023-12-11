e60ad7827f112e8d4b2b0d352667245693e54b1c
stellaljung <stella_ljung@brown.edu>
2023-12-11 15:09:42 -0500

    refactor: change server test to use leaked byte table

--- backend/tests/server.cpp
+++ backend/tests/server.cpp
@@ -27,8 +27,17 @@ TEST_CASE("Test endpoints using handler")
 
     // create the database, with tables passwords and secret
     database::Database db = database::Database(path, true);
-    REQUIRE_NOTHROW(db.execute("CREATE TABLE passwords (password TEXT);"));
-    REQUIRE_NOTHROW(db.execute("CREATE TABLE secret (key TEXT);"));
+
+    // offset constant
+    int offset = 1;
+
+    // create all tables from 0 to (2^8)^offset-1
+    for (int i = 0; i < std::pow(std::pow(2, 8), offset); i++)
+    {
+        std::string query_str = "table" + std::to_string(i);
+        db.execute("CREATE TABLE " + query_str + " (password TEXT);");
+        spdlog::info("Table key: {}", query_str);
+    }
 
     // create a mock password set
     std::unordered_set<std::string> passwords = {"TestPass1&", "ChocolateCake1!", "LoveMyDogs3$"};
@@ -38,19 +47,32 @@ TEST_CASE("Test endpoints using handler")
     crypto_core_ristretto255_scalar_random(b);
 
     // 2. encrypt each password with b (and hash to point)
-    const size_t offset = 1;
     std::vector<std::string> encrypted_passwords = cryptography::encrypt(passwords, b, offset);
 
     // 3. insert into database
     for (const auto &password : encrypted_passwords)
     {
+        // determine which table to insert into based on leaked byte
+        std::string encoded_byte = crow::utility::base64encode(password.substr(0, offset), offset);
+        unsigned int table_num = static_cast<int>(encoded_byte[0]);
+        std::string table_str = std::to_string(table_num);
+        std::string query_str = "table" + table_str;
+        spdlog::info("Password Table: {}", query_str);
+
+        std::string raw_password = password.substr(offset, password.size() - offset);
+        spdlog::info("Password str for above: {}", raw_password);
+
         // encode password before inserting into database
-        db.execute("INSERT INTO passwords (password) VALUES ('" + crow::utility::base64encode(password, password.size()) + "');");
+        db.execute("INSERT INTO " + query_str + " (password) VALUES ('" + crow::utility::base64encode(raw_password, raw_password.size()) + "');");
     }
+
+    // create key table
+    db.execute("CREATE TABLE secret (key TEXT);");
 
     // encode key b and insert into database
     db.execute("INSERT INTO secret (key) VALUES ('" + crow::utility::base64encode(std::string(reinterpret_cast<const char *>(b), crypto_core_ristretto255_SCALARBYTES), crypto_core_ristretto255_SCALARBYTES) + "');");
 
+    // initialize endpoints
     server::breachedPasswords(app, db, offset);
 
     // check that all the route handlers were created
@@ -73,38 +95,27 @@ TEST_CASE("Test endpoints using handler")
         req.method = "POST"_method;
         req.add_header("Access-Control-Allow-Headers", "*");
         req.add_header("Content-Type", "application/json");
-        req.body = "TestPass1&";
+
+        // encrypt user password with b (and hash to point)
+        std::string user_request = "TestPass1&";
+        std::string encrypted_password = cryptography::hashAndEncryptPassword(user_request, b, offset);
+        req.body = crow::utility::base64encode(encrypted_password, encrypted_password.size());
 
         app.handle(req, res);
 
         auto body = nlohmann::json::parse(res.body);
         CHECK(body["status"] == "success");
 
+        // check correct bucket is returned
         REQUIRE(body["breachedPasswords"].is_array());
-        std::vector<std::string> breached_passwords = body["breachedPasswords"].get<std::vector<std::string>>();
-        CHECK(breached_passwords.size() == 3);
-        for (const auto &breached_password : breached_passwords)
-        {
-            // offset determines the amount of padding appended to the end of the password
-            switch (offset % 3)
-            {
-            case 0:
-                // password is padded with single '='
-                CHECK(breached_password[breached_password.size() - 1] == '=');
-                CHECK(breached_password[breached_password.size() - 2] != '=');
-                break;
-            case 1:
-                // password is not padded
-                CHECK(breached_password[breached_password.size() - 1] != '=');
-                CHECK(breached_password[breached_password.size() - 2] != '=');
-                break;
-            case 2:
-                // password is padded with two '='
-                CHECK(breached_password[breached_password.size() - 1] == '=');
-                CHECK(breached_password[breached_password.size() - 2] == '=');
-                break;
-            }
-        }
+        std::vector<std::string> breached_bucket = body["breachedPasswords"].get<std::vector<std::string>>();
+        CHECK(breached_bucket.size() == 1);
+
+        // check breached password encoding
+        std::string breached_password = breached_bucket[0];
+        CHECK(breached_password[breached_password.size() - 1] == '=');
+
+        // check user password encoded
         std::string user_password = body["userPassword"];
         CHECK(!user_password.empty());
         CHECK(user_password.back() == '=');
